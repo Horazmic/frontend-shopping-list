4f5bdd9f3614c37f251f02afdbc8e95a9b6b9eba
MIhola02 <mikihore@gmail.com>
2024-10-23 18:13:37 +0200

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -1,14 +1,23 @@
 function About() {
-    return (
-        <div className="flex min-h-screen bg-gray-100">
-            <div className="container mx-auto px-4 py-8 bg">
-                <h1 className="text-3xl font-bold mb-6">About This Shopping List App</h1>
-                <p className="text-lg">
-                    Welcome to our Shopping List App! This application is designed to simplify your shopping experience by allowing you to create, manage, and share shopping lists with ease. Whether you're planning for weekly groceries or organizing a special event, our app helps you stay organized and efficient. With features like collaborative list editing and user management, you can streamline your shopping process and never forget an important item again.
-                </p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      <div className="container mx-auto px-4 py-8 bg">
+        <h1 className="text-3xl font-bold mb-6">
+          About This Shopping List App
+        </h1>
+        <p className="text-lg">
+          Welcome to our Shopping List App! This application is designed to
+          simplify your shopping experience by allowing you to create, manage,
+          and share shopping lists with ease. Whether you're planning for weekly
+          groceries or organizing a special event, our app helps you stay
+          organized and efficient. With features like collaborative list editing
+          and user management, you can streamline your shopping process and
+          never forget an important item again.
+        </p>
+        <p>test</p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
